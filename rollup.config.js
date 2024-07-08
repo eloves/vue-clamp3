69b6af8f8fdc46b8e5e184918b8411a07c11e63c
xezou <xezou@pisx.com>
2024-07-08 15:20:53 +0800

    feat: 打包更新

--- rollup.config.js
+++ rollup.config.js
@@ -3,41 +3,28 @@ import buble from '@rollup/plugin-buble'
 import { terser } from 'rollup-plugin-terser'
 import resolve from '@rollup/plugin-node-resolve'
 
-export default [{
-  input: 'src/components/Clamp.js',
-  output: {
-    file: 'dist/vue-clamp.js',
-    name: 'VueClamp',
-    format: 'umd',
-    globals: {
-      vue: 'Vue',
-      'resize-detector': 'resizeDetector'
-    }
+export default [
+  {
+    input: 'src/components/index.js',
+    output: {
+      file: 'lib/vue-clamp.js',
+      name: 'VueClamp3',
+      format: 'umd',
+      globals: {
+        vue: 'Vue',
+        'resize-detector': 'resizeDetector'
+      }
+    },
+    external: ['vue', 'resize-detector'],
+    plugins: [resolve(), vue(), buble(), terser()]
   },
-  external: [
-    'vue',
-    'resize-detector'
-  ],
-  plugins: [
-    resolve(),
-    vue(),
-    buble(),
-    terser()
-  ]
-}, {
-  input: 'src/components/Clamp.js',
-  output: {
-    file: 'dist/vue-clamp.esm.js',
-    format: 'es'
-  },
-  external: [
-    'vue',
-    'resize-detector'
-  ],
-  plugins: [
-    resolve(),
-    vue(),
-    buble(),
-    terser()
-  ]
-}]
+  {
+    input: 'src/components/index.js',
+    output: {
+      file: 'lib/vue-clamp.esm.js',
+      format: 'es'
+    },
+    external: ['vue', 'resize-detector'],
+    plugins: [resolve(), vue(), buble(), terser()]
+  }
+]
